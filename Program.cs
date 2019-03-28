715d4f87e12bd61afd507e1af9aa1a6fc716e602
Константин Дьяченко <konard@me.com>
2019-03-28 21:51:12 +0300

    First code upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Pegasus;
+
+namespace PegasusExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new Parsers.Parser();
+            var result = parser.Parse("5,7+8,9*42");
+            Console.WriteLine(result);
+        }
+    }
+}
